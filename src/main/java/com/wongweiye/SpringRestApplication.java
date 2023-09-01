99cd10e8ca0be89d10e145051e352c6349ee0d2f
Edwardwong0826 <edwardwong_94@outlook.com>
2023-09-02 01:36:12 +0800

    Spring Security Authenticate and Authorize with JWT
    Add RSA key for manually generate keypair.pem 2048 for public key and private key

--- src/main/java/com/wongweiye/SpringRestApplication.java
+++ src/main/java/com/wongweiye/SpringRestApplication.java
@@ -1,9 +1,12 @@
 package com.wongweiye;
 
+import com.wongweiye.security.RSAKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
+@EnableConfigurationProperties(RSAKeyProperties.class)
 @SpringBootApplication
 public class SpringRestApplication {
 
